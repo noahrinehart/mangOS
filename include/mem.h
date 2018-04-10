cabbad2cd80455f3d1c8ce85497ab77af1c97469
Noah Rinehart <rinehart.noah@gmail.com>
2018-04-10 17:31:48 -0400

    updated linker address

--- include/mem.h
+++ include/mem.h
@@ -10,4 +10,12 @@
 void memset(uint8 *dest, uint8 val, uint32 len);
 void memcpy(uint8 *dest, const uint8 *src, uint32 len);
 
+extern int text;  /* Start of text segment  */
+extern int etext; /* End of text segment    */
+extern int data;  /* Start of data segment  */
+extern int edata; /* End of data segment    */
+extern int bss;   /* Start of bss segment   */
+extern int ebss;  /* End of bss segment   */
+extern int end;   /* End of program   */
+
 #endif // MEM_H
--- src/kmain.c
+++ src/kmain.c
@@ -4,15 +4,23 @@
 #include <timer.h>
 #include <keyboard.h>
 #include <multiboot.h>
+#include <mem.h>
 
 void kmain(struct multiboot_header *mboot_ptr)
 {
   init_descriptors();
 
   vga_clear();
+  vga_write("test");
 
   asm volatile("sti"); // Enable interrupts
   init_keyboard();
+
+  vga_write("Start of text: ");
+  vga_write_hex(text);
+  vga_put('\n');
+
+
 
   for(;;);
 }
