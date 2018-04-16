c8323e517e63f473e8f50e428881d53b678debbb
Noah Rinehart <rinehart.noah@gmail.com>
2018-04-16 14:18:36 -0400

    updated keyboard scancodes

--- src/keyboard.c
+++ src/keyboard.c
@@ -3,42 +3,43 @@
 #include <keyboard.h>
 #include <vga.h>
 
-unsigned char kbdus[128] = {
-    0,    27,  '1', '2', '3',  '4', '5', '6', '7',  '8', /* 9 */
-    '9',  '0', '-', '=', '\b',                           /* Backspace */
-    '\t',                                                /* Tab */
-    'q',  'w', 'e', 'r',                                 /* 19 */
-    't',  'y', 'u', 'i', 'o',  'p', '[', ']', '\n',      /* Enter key */
-    0,                                                   /* 29   - Control */
-    'a',  's', 'd', 'f', 'g',  'h', 'j', 'k', 'l',  ';', /* 39 */
-    '\'', '`', 0,                                        /* Left shift */
-    '\\', 'z', 'x', 'c', 'v',  'b', 'n',                 /* 49 */
-    'm',  ',', '.', '/', 0,                              /* Right shift */
-    '*',  0,                                             /* Alt */
-    ' ',                                                 /* Space bar */
-    0,                                                   /* Caps lock */
-    0,                                                   /* 59 - F1 key ... > */
-    0,    0,   0,   0,   0,    0,   0,   0,   0,         /* < ... F10 */
-    0,                                                   /* 69 - Num lock*/
-    0,                                                   /* Scroll Lock */
-    0,                                                   /* Home key */
-    0,                                                   /* Up Arrow */
-    0,                                                   /* Page Up */
-    '-',  0,                                             /* Left Arrow */
-    0,    0,                                             /* Right Arrow */
-    '+',  0,                                             /* 79 - End key*/
-    0,                                                   /* Down Arrow */
-    0,                                                   /* Page Down */
-    0,                                                   /* Insert Key */
-    0,                                                   /* Delete Key */
-    0,    0,   0,   0,                                   /* F11 Key */
-    0,                                                   /* F12 Key */
-    0, /* All other keys are undefined */
+uint8
+    kbdus[128] =
+        {
+            0,   /* None */
+            0,   /* Esc  */
+            '1', /* 1    */
+            '2', /* 2    */
+            '3',  '4',  '5',  '6', '7', '8', '9',  '0', '-',
+            '=',  '\b', '\t', 'q', 'w', 'e', 'r',  't', 'y',
+            'u',  'i',  'o',  'p', '[', ']', '\n', 0, /* Left control */
+            'a',  's',  'd',  'f', 'g', 'h', 'j',  'k', 'l',
+            ';',  '\'', '`',  0, /* Left Shift */
+            '\\', 'z',  'x',  'c', 'v', 'b', 'n',  'm', ',',
+            '.',  '/',  0, /* Right Shift */
+            '*',  0,       /* Left alt */
+            ' ',  0,       /* CapsLock */
+            0,             /* F1 */
+            0,             /* F2 */
+            0,             /* F3 */
+            0,             /* F4 */
+            0,             /* F5 */
+            0,             /* F6 */
+            0,             /* F7 */
+            0,             /* F8 */
+            0,             /* F9 */
+            0,             /* F10 */
+            0,             /* NumLock */
+            0,             /* ScrollLock */
+            '7',           /* Keypad 7 */
+            '8',  '9',  '-',  '4', '5', '6', '+',  '1', '2',
+            '3',  '0',  '.',  0,   0,   0,   0, /* F11 */
+            0                                   /* F12 */
 };
 
 /* Can take registers_t regs as parameter */
 static void keyboard_handler() {
-  unsigned char scancode = inb(0x60);
+  uint8 scancode = inb(0x60);
 
   if (scancode & 0x80) {
   } else {
